8d7bbd493a0540ac0dec386f1b469414551aff5c
Juvekar, Chiraag <chiraag.juvekar@gmail.com>
2018-11-26 04:28:44 -0500

    Preprocessing and Resnet scripts

---
+++ util.py
@@ -0,0 +1,21 @@
+dataset_base = "d:/datasets/diabetic/"
+train_path = dataset_base+"train/"
+test_path = dataset_base+"test/"
+trainLabels = dataset_base+"trainLabels.csv"
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
